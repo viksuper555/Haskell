9a249723efc84c752254141d8d243297e994a311
Viktor Kolev <viksuper555@gmail.com>
2019-11-17 11:33:53 +0200

    Optimised

--- exam2.hs
+++ exam2.hs
@@ -1,23 +1,4 @@
-nextSpaceIndex str separator i = 
-    if null str
-        then -1
-    else if (take (length separator) str) == separator
-        then i
-    else nextSpaceIndex (tail str) separator (i + 1)
-
-split str separator =
-    if (null str) || ((nextSpaceIndex str separator 0) == -1)
-        then [str]
-    else (take (nextSpaceIndex str separator 0) str) : (split (drop ((nextSpaceIndex str separator 0) + (length separator)) str) separator)
-
 rollLeft array times = (drop times array) ++ (take times array)
-
-mapArray array mapper =
-    if null array
-        then []
-    else (mapper (head array)) : (mapArray (tail array) mapper)
-
-
 
 digits :: Integer -> [Int]
 digits n = map (\x -> read [x] :: Int) (show n)
